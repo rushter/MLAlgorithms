04c47f694310a9f6bf42f9835b4253ad9c494d6c
rushter <me@rushter.com>
2016-11-18 02:01:26 +0300

    FIx: check if X is numpy array

--- mla/base/base.py
+++ mla/base/base.py
@@ -51,6 +51,9 @@ class BaseEstimator(object):
         self._setup_input(X, y)
 
     def predict(self, X=None):
+        if not isinstance(X, np.ndarray):
+            X = np.array(X)
+
         if self.X is not None:
             return self._predict(X)
         else:
