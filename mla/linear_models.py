Fix the formula of l2 penalty

mla/linear_models.py
@@ -50,7 +50,7 @@ class BasicRegression(BaseEstimator):
         if self.penalty == "l1":
             loss += self.C * np.abs(w[1:]).sum()
         elif self.penalty == "l2":
-            loss += (0.5 * self.C) * (w[1:] ** 2).mean()
+            loss += (0.5 * self.C) * (w[1:] ** 2).sum()
         return loss
 
     def _cost(self, X, y, theta):